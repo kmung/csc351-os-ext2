295644f3bc44d6b2e56bbec0fdf73f01b2a7b657
Khawm Mung <johnmungtawng5@gmail.com>
2023-10-24 11:56:17 -0500

    cpp class

--- app/fs/fs.cpp
+++ app/fs/fs.cpp
@@ -4,6 +4,8 @@
 
 #include "fs.h"
 
+using namespace std;
+
 int main(int argc, char **argv) {
   return 0;
 }
--- app/shell/shell.h
+++ app/shell/shell.h
@@ -1,6 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+class shell {
+  private:
+    /* data */
+
+  public:
+    // constructor
+    shell(int argc, char *argv[]);
+    // destructor
+    ~shell();
+};
 
 
 #endif
