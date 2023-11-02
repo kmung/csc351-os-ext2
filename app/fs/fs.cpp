5658149e41e2564f96364c2dd8663cc5f21f532c
ssyakmu <ssyakmu@gmail.com>
2023-11-02 16:49:50 -0500

    Create disk

--- app/fs/fs.cpp
+++ app/fs/fs.cpp
@@ -4,6 +4,74 @@
 
 #include "fs.h"
 
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <vector>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+
+int create_disk(){
+	// 2GB memory size
+	const uint32_t memory_size = 2ULL * 1024 * 1024 * 1024;
+
+	// 4KB block size
+	const size_t block_size = 4 * 1024;
+
+	// Calculate the number of blocks
+	size_t num_blocks = memory_size / block_size;
+
+	// Allocate 2GB memory
+	uint32_t *memory = static_cast<uint32_t*>(malloc(memory_size));
+
+	if (memory == NULL) {
+		printf("Memory allocation failed.\n");
+	}
+
+	// Initialize memory?
+	// memset(memory, 0, memory_size);
+
+	// Create an array to store block addresses
+	uint32_t **block_addresses = static_cast<uint32_t**>(malloc(num_blocks * sizeof(uint32_t*)));
+
+	// Store each block address
+    for (size_t i = 0; i < num_blocks; i++) {
+        block_addresses[i] = memory + i * block_size;
+    }
+
+	// Initialize super block
+	struct SuperBlock sb;
+
+	// Initialize arrays to keep track of free data block and inode
+	sb.block_freelist = new bool[sb.nBlocks];
+	sb.inode_freelist = new bool[sb.nInodes];
+
+	// Mark first block to true
+	sb.block_freelist[0] = true;
+
+    // Marking all data bloks as free except the first block
+    for (int i = 1; i < sb.nBlocks; i++)
+        sb.block_freelist[i] = false; 
+
+    // Marking all inodes as free 
+    for (int i = 0; i < sb.nInodes; i++)
+        sb.inode_freelist[i] = false;
+
+
+	// Todo list
+	// Allocate Super block on the first block
+	// Initialize Inode
+	// Mark inode of superblock
+}
+
+
+
 int main(int argc, char **argv) {
-  return 0;
+	return 0;
 }
--- app/fs/fs.h
+++ app/fs/fs.h
@@ -1,46 +1,170 @@
 #ifndef FS_H
 #define FS_H
+#include <string>
+#include <ctime>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <iostream>
+#include <time.h>
 
-//ext2 based fs
-struct fs {
-  unsigned long inodes_per_block; // number of inodes per block
-  unsigned long blocks_per_group; // number of blocks in a group
-  unsigned long inodes_per_group; // number of inodes in a group
-  unsigned long group_desc_count; // number of group descriptor blocks
-  unsigned long desc_per_block; // number of group descriptors per block
-  unsigned long groups_count; // number of groups in the fs
+
+#include "bitmap.h"
+#include "mm.h"
+#include "memory.h"
+
+using namespace std;
+
+#define MEMORY_SIZE = 2ULL * 1024 * 1024 * 1024;
+#define BLOCK_SIZE = 4 * 1024;
+#define INODE_SIZE = 128;
+#define NINODES = 524288;
+
+
+
+// Super block locates on the first block of the memory
+// It contains the entire information of file system
+// Although the size of superblock is 1KB, it still use the entire space of first block
+struct SuperBlock {
+	// 2GB Memory Size
+	const uint32_t memory_size = 2ULL * 1024 * 1024 * 1024;
+
+	// 4KB block size
+	const size_t block_size = 4 * 1024;
+
+	// Total number of disk blocks
+	const int nBlocks = memory_size / block_size;
+
+	// Number of Inode blocks, we should decide it
+	// I put half of nblocks for now
+	const int nInodes = 524288;
+
+	// Size of inode strcuture
+	const size_t inode_size = 128;
+
+
+	// List to keep track of free inode and datablock
+	// If data exists in block, return true. Otherwise return false
+	bool* inode_freelist;
+    bool* block_freelist; 
 };
 
-// FS date
-#define FS_DATE "10/24/2023"
 
-// special inode numbers
-#define FS_BAD_INO 1 // bad blocks inode
-#define FS_ROOT_INO 2 // Root inode
-#define FS_BOOT_LOADER_INO 5 // boot loader inode
-#define FS_UNDEL_DIR_INO 6 // undelete directory inode
 
-// structure of the super block
-struct fs_super_block
-{
-  // inodes count
-  // blocks count
-  // free blocks count
-  // free inodes count
-  // mount time
-  // write time
-  // default uid for reserved blocks
-  // default gid for reserved blocks
+
+
+// Group descriptor contains information of each block group ex)superblock, bitmap
+// It should locate next to super block
+// But I am not sure we are using it
+struct GroupDesc {
+	// Describe where bitmaps are located
+	uint32_t block_bitmap;
+	uint32_t inode_bitmap;
+
+	// Describe first index of inode_table
+	uint32_t inode_table;
+
+	// Count free blocks and inodes
+	int nFreeBlocks;
+	int nFreeInodes;	
 };
 
-// structure of directory entry
-struct fs_dir_entry {
-  // inode number
-  // directory entry length
-  // name length
-  // file type
-  // file name
+
+// Each inode points certain data block
+// Inode contains information about data block
+struct Inode {
+	// File mode and access permission
+	uint16_t f_mode;
+
+	// Number of hard links to the file
+	uint16_t Nlink;
+
+	// File owner id
+	uint16_t Uid;
+
+	// Group the file belongs to
+	uint16_t Gid;
+
+	// File size in bytes
+	uint32_t FileSize;
+
+	// Creation time
+	time_t Ctime;
+
+	// Time of last access
+	time_t Atime;
+
+	// Time of last modification
+	time_t Mtime;
+
+	// Include the index where file locates within data blocks
+	// If we are not making a huge file system, we can just use direct blocks
+	uint32_t DirectBlocks[12];
 };
+
+
+
+
+// I created the array to keep track on data block and inodes
+// I am not sure we still need to use bitmap
+
+// Each bitmap is one block long
+// Explanation about bitmap is included on bitmap.h file
+struct BlockBitmap {
+	const size_t bitmapSize = 1 * 1024 * 1024;
+	BlockBitmap();
+};
+
+
+
+
+struct InodeBitmap{
+	const size_t bitmapSize = 1 * 1024 * 1024;
+	InodeBitmap();
+};
+
+
+
+
+// Save iNode with file name
+// Save the entry to subdirectory if exists
+
+// Ex)
+// Directory Entries in /home/userA:
+// - Entry for ".": Inode Number 12345 (refers to /home/userA itself)
+// - Entry for "..": Inode Number 56789 (refers to the parent directory of /home/userA)
+// - Entry for "file1.txt": Inode Number 23456 (refers to a file named "file1.txt" in /home/userA)
+// - Entry for "subdir": Inode Number 78901 (refers to a subdirectory named "subdir" in /home/userA)
+
+// Direcotry Entries in /home/userA/file1.txt
+// Entry for "file1.txt": Inode Number 23456
+
+// Define max length of name
+#define EXT2_NAME_LEN 256
+
+struct dentry{
+	int inode;
+	size_t entrySize;
+	int ftype;	// ex) file or directory
+	char fname[EXT2_NAME_LEN];
+};
+
+
+// Functiont to allocate memory to our disk partiton. 
+int create_disk();
+
+
+
+class fs {
+	private:
+		mode_t fileType : 3;
+		unsigned short OwnerPermissions : 3;
+		unsigned short GroupPermissions : 3;
+		unsigned short OthersPermissions : 3;
+		unsigned short SpecialPermissions : 3;
+};
+
+
+
 
 
 #endif
