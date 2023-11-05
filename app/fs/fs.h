a0ecf80ac08174d0c73ed3fad2d2fa3b1c0e89b9
Rita Njoroge <ritanjoroge@Ritas-MacBook-Air.local>
2023-11-04 19:27:20 -0500

    Bruh

--- app/fs/fs.h
+++ app/fs/fs.h
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#include "inode.h"
+//#include "inode.h"
 
 // FS date
 #define FS_DATE "10/24/2023
@@ -25,7 +25,7 @@ class fs {
 
 		// refer to https://man7.org/linux/man-pages/man2/syscalls.2.html for more
 		int my_creat(const char *pathname, mode_t mode);
-		int my_open(const char *pathname);
+		int my_open(const char *pathname, mode_t mode);
 		ssize_t read(int fd, void buf[.count], size_t count);
 		ssize_t write(int fd, const void buf[.count], size_t count);
 
