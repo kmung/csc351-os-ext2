76ba72263d4c919b623bf2801bf2c6e1b3fdd9bb
Rita Njoroge <ritanjoroge@Ritas-Air.attlocal.net>
2023-11-05 22:06:40 -0600

    Stats work and i changed the main to test them.

--- app/fs/fs.cpp
+++ app/fs/fs.cpp
@@ -17,58 +17,66 @@ using namespace std;
 
 #include "fs.h"
 
-int my_read(ifstream& file, char* buffer, int nbytes) {
-  //Pass in fd 'file' along with buffer and number of bytes to read
-  file.read(buffer, nbytes);
-  //OS manages the file pointer which tracks current position in the file, reads file and updates pointer
-  return file.gcount();
-  //returns number of bytes read
-}
+int fs::my_read(int fd, char* buffer, int nbytes) {
+    // Use the read function with the file descriptor to read data.
+    int bytesRead = read(fd, buffer, nbytes);
+    if (bytesRead == -1) {
+        // Handle the error, e.g., print an error message.
+        std::cerr << "Error while reading from file descriptor." << std::endl;
+    }
 
-int my_write(ofstream& file, const char* buffer, int nbytes) {
-  //Pass in fd, the data to write, and number of bytes to write
-  file.write(buffer, nbytes);
-  //file pointer is managed to be at the correct position, if not specified it will write to the end of the file
-  //specify and offset and it writes to that position
-  return file.good() ? nbytes : 0;
-  //returns number of bytes written 
-} 
-
-//******************************************************************************
-
-int my_creat(const string& name, int mode) {
-  filesystem::path filePath(name); //library
-  int rc = 0;
-  //check to see if file already exists
-  if(filesystem::exists(filePath)) {
-    rc = 2; //file exists
-  } 
-  //if file doesnt exist, create it (inode implementation?)
-  ofstream file(name, ios::out); 
-  if (!file.is_open()) {
-    rc = -1;
-  }
-  file.close();
-  return rc;
+    return bytesRead;
 }
 
 //******************************************************************************
 
-int my_Iseek(fstream& file, streamoff offset, ios_base::seekdir when) {
-  file.seekg(offset, when); //use seekg for input
-  int rc = 0;
+int fs::my_write(int fd, const char* buffer, int nbytes) {
+    // Use the write function with the file descriptor to write data.
+    int bytesWritten = write(fd, buffer, nbytes);
 
-  if(file.fail()) {
-    cerr << "Seek failed" << endl;
-    rc = -1;
-  }
+    if (bytesWritten == -1) {
+        // Handle the error, e.g., print an error message.
+        std::cerr << "Error while writing to file descriptor." << std::endl;
+    }
 
-  return rc;
+    return bytesWritten;
 }
 
 //******************************************************************************
 
-int my_open(const char *pathname, mode_t mode) {
+int fs::my_creat(const string& name, int mode) {
+    int fd = open(name.c_str(), O_CREAT | O_WRONLY | O_TRUNC, mode);
+    int rc = 0;
+
+    if (fd == -1) {
+        // Handle the error, e.g., print an error message.
+        std::cerr << "Error creating file." << std::endl;
+        rc = -1;
+    }
+
+    // Successfully created the file.
+    close(fd); // Close the file descriptor.
+    return rc;
+}
+
+//******************************************************************************
+
+int fs::my_Iseek(int fd, off_t offset, int when) {
+    // Use the lseek function with the file descriptor to seek.
+    off_t result = lseek(fd, offset, when);
+    int rc = 0;
+    if (result == -1) {
+        // Handle the error, e.g., print an error message.
+        std::cerr << "Seek failed" << std::endl;
+        rc = -1;
+    }
+
+    return rc; 
+}
+
+//******************************************************************************
+
+int fs::my_open(const char *pathname, mode_t mode) {
     //try opening the file first
     int fd = open(pathname, mode);
 
@@ -93,36 +101,36 @@ int my_open(const char *pathname, mode_t mode) {
 //******************************************************************************
 
 int my_close(int file_descriptor) {
-	int errC;
-	//check if file descriptor is valid since it can only be 0,1 or 2
-	if (file_descriptor < 0) {
-		cout << "Invalid file descriptor!" << endl;
-	} else {
-		errC = close(file_descriptor);
-		if (errC == -1) {
-		//using the c library, we can error handle and see if the most recent
-		//function call occured an error and is set to defined numerical value
-		//recognized by the OS  to show that the file doesn't exist
-			if (errno == ENOENT) { 
-				cout << "ERROR! File does not exist!" << endl;
-			} else { //another possible error
-				cout << "Cannot close file!" << endl;
-			}
-    	} else {
-			cout << "File closed successfully!" << endl;
-			errC = 0;
-		}
-	}
+    int errC;
+    //check if file descriptor is valid since it can only be 0,1 or 2
+    if (file_descriptor < 0) {
+	    cout << "Invalid file descriptor!" << endl;
+    } else {
+	    errC = close(file_descriptor);
+	    if (errC == -1) {
+	    //using the c library, we can error handle and see if the most recent
+	    //function call occured an error and is set to defined numerical value
+	    //recognized by the OS  to show that the file doesn't exist
+		    if (errno == ENOENT) { 
+			    cout << "ERROR! File does not exist!" << endl;
+		    } else { //another possible error
+			    cout << "Cannot close file!" << endl;
+		    }
+	    } else {
+		    cout << "File closed successfully!" << endl;
+		    errC = 0;
+	    }
+    }
 
-	return errC;
+    return errC;
 }
 
 //******************************************************************************
 //the function will obtaion the information about the named file and
 //write it to the area pointed to by the buf argument
 // the buf argument passed in is a pointer to a stat structure
-int my_stat(const char *pathname, struct stat buf) {
-	int errC;
+int my_stat(const char *pathname, struct stat& buf) {
+	int errC = 0;
 
 	if (stat(pathname, &buf) == 0) {
 		//meaning it was successfull print the information
@@ -135,23 +143,75 @@ int my_stat(const char *pathname, struct stat buf) {
 		cout << "File size:" << buf.st_blksize << endl;
 		//for the time_t values, we will extract each one by one and convert them into readable format
 		//which can then be printed out
-		char *accessT;
-		char *modT;
-		char *createT;
+		char accessT[15];
+		char modT[15];
+		char createT[15];
 
-		struct tm *at;
-		struct tm *mt;
-		struct tm *ct;
+        //use local time() to get the time_t variables of each desired file time
+		tm *access_time = localtime(&buf.st_atime);
+		tm *mod_time = localtime(&buf.st_mtime);
+		tm *create_time = localtime(&buf.st_ctime);
 
-		at = localtime(&buf.st_atimespec);
-		strftime(accessT, 15, "%Y-%m-%d %H:%M:%S", localtime(&buf.st_atimespec));
-		strftime(modT, 15, "%Y-%m-%d %H:%M:%S", localtime(&buf.st_mtimespec));
-		strftime(createT, 15, "%Y-%m-%d %H:%M:%S", localtime(&buf.st_ctimespec));
+        //put the date in the correct format and then print
+		strftime(accessT, 15, "%Y-%m-%d %H:%M:%S", access_time);
+		strftime(modT, 15, "%Y-%m-%d %H:%M:%S", mod_time);
+		strftime(createT, 15, "%Y-%m-%d %H:%M:%S", create_time);
 
-	}
+        cout << "Access Time:" << accessT << endl;
+        cout << "Modification Time:" << modT << endl;
+        cout << "Creation Time:" << createT << endl;
+
+	} else {
+        errC = -1;
+    }
+
+    return errC;
 }
 
+//******************************************************************************
 
+//the function shall obtain information about about a file
+//with the specififed fd and write it to the are pointed
+//to by buf
+int fs::my_fstat(int fd, struct stat& buf) {
+    int errC = 0;
+    if (fstat(fd, &buf) == 0) {
+        //meaning it was successfull therefore print information about the open file
+        cout << "Device name:" << buf.st_dev << endl;
+		cout << "Inode:" << buf.st_ino << endl;
+		cout << "Mode:" << buf.st_mode << endl;
+		cout << "Owner:" << buf.st_uid <<endl;
+		cout << "Number of links:" << buf.st_nlink <<endl;
+		cout << "Group:" << buf.st_gid << endl;
+		cout << "File size:" << buf.st_blksize << endl;
+		//for the time_t values, we will extract each one by one and convert them into readable format
+		//which can then be printed out
+		char accessT[15];
+		char modT[15];
+		char createT[15];
+
+        //use local time() to get the time_t variables of each desired file time
+		tm *access_time = localtime(&buf.st_atime);
+		tm *mod_time = localtime(&buf.st_mtime);
+		tm *create_time = localtime(&buf.st_ctime);
+
+        //put the date in the correct format and then print
+		strftime(accessT, 15, "%Y-%m-%d %H:%M:%S", access_time);
+		strftime(modT, 15, "%Y-%m-%d %H:%M:%S", mod_time);
+		strftime(createT, 15, "%Y-%m-%d %H:%M:%S", create_time);
+
+        cout << "Access Time:" << accessT << endl;
+        cout << "Modification Time:" << modT << endl;
+        cout << "Creation Time:" << createT << endl;
+
+        //close file descriptor
+        my_close(fd);
+
+    } else {
+        errC = -1;
+    }
+
+    return errC;
+}
 
 //******************************************************************************
-// open function 
--- app/fs/fsm.cpp
+++ app/fs/fsm.cpp
@@ -1,11 +1,64 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <filesystem>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include "fs.h"
-
 using namespace std;
 
-int main(int argc, char **argv) {
-    return 0;
+
+
+int main() {
+
+    fs myFs;
+    struct stat fileStat; //for stat functions
+    
+    // Test my_read and my_write functions with placeholder data
+    char dataToWrite[] = "This is a test.";
+    int writeFd = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    int bytesWritten = myFs.my_write(writeFd, dataToWrite, strlen(dataToWrite));
+
+    // Check if writing was successful
+    if (bytesWritten > 0) {
+        cout << "Successfully wrote data." << endl;
+    } else {
+        cerr << "Error writing to the file." << endl;
+    }
+
+    close(writeFd);
+
+    // Test my_creat function
+    int createResult = myFs.my_creat("newfile.txt", 0644);
+    if (createResult == 0) {
+        cout << "File created successfully." << endl;
+    } else if (createResult == 2) {
+        cout << "File already exists." << endl;
+    } else if (createResult == -1) {
+        cout << "Error creating file." << endl;
+    }
+
+    // Test my_open and my_close functions
+    int openFd = myFs.my_open("example.txt", O_RDONLY);
+    if (openFd != -1) {
+        myFs.my_close(openFd);
+    } else {
+        cerr << "Error opening the file." << endl;
+    }
+
+    // Test my_stat function
+    int statResult = myFs.my_stat("example.txt", fileStat);
+    if (statResult == -1) {
+        cerr << "Error getting file status." << endl;
+    }
+
+    //Testing my_fstat function
+    //use the file descriptor from the open function
+    int fstatResult = myFs.my_fstat(openFd, fileStat);
+    if (statResult == -1) {
+        cerr << "Error getting file status." << endl;
+    }
 }
