1cc6e441db8b9b5c3fc4fe67cd7d312b21c14a1b
Khawm Mung <johnmungtawng5@gmail.com>
2023-12-04 21:52:52 -0600

    shell formatted

--- fs/shell.cpp
+++ fs/shell.cpp
@@ -3,32 +3,76 @@
 #include <cstdlib>
 #include <iostream>
 #include <cassert>
-#include <unistd.h>
-#include <syscall.h>
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
-#include <arpa/inet.h>
+#include <sys/ioctl.h>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
+#include <vector>
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+    #include <arpa/inet.h> 
+#endif
 
 using namespace std;
 
 #define SERVER_PORT 8080
 #define MAX_BUFFER_SIZE 4096
 
+std::vector<std::string> splitString(const std::string& s, const std::string& delimiters) {
+    std::vector<std::string> tokens;
+    size_t start = 0, end = 0;
+
+    while ((end = s.find_first_of(delimiters, start)) != std::string::npos) {
+        tokens.push_back(s.substr(start, end - start));
+        start = end + 1;
+    }
+
+    tokens.push_back(s.substr(start));
+    return tokens;
+}
+
+string replaceWord(std::string& str, const std::string& target, const std::string& replacement) {
+    size_t pos = 0;
+    while ((pos = str.find(target, pos)) != std::string::npos) {
+        str = str.replace(pos, target.length(), replacement);
+        pos += replacement.length();
+    }
+    return str;
+}
+
+int getTerminalWidth() {
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  return w.ws_col;
+}
+
 // clearing the shell using escape sequences
 void clearShell() {
   cout << "\033[H\033[J";
 }
 
+// center-align text within the terminal window
+void printCentered(const string& text) {
+  int terminalWidth = getTerminalWidth();
+  int padding = (terminalWidth - text.length()) / 2;
+
+  cout << setw(padding + text.length()) << text << endl;
+}
+
 // shell greeting during start up
 void init_shell() {
-  clearShell();
-  cout << "\n\n\n\n\t********************************************" << endl;
-  cout << "\n\n\t*****The Creative Awesome Shell - Crash*****" << endl;
-  cout << "\n\n\t*****Proceed with caution...*****" << endl;
-  cout << "\n\n\t********************************************\n\n" << endl;
+    clearShell();
+    cout << "\n\n\n\n";
+
+    printCentered("********************************************");
+    printCentered("**** The Creative Awesome Shell - Crash ****");
+    printCentered("**** Proceed with caution... ***************");
+    printCentered("********************************************\n");
 }
 
 
@@ -37,8 +81,11 @@ void init_shell() {
   */
 void repl(int sock) {
   char buffer[MAX_BUFFER_SIZE];
-  char cwd[MAX_BUFFER_SIZE]; // to store the current working directory
+  // char cwd[MAX_BUFFER_SIZE]; // to store the current working directory
 
+  string cwd = "";
+
+  bool firstTimeCalled = true;
   while (true) {
     // cout << "breaking" <<endl;
     // break;
@@ -52,21 +99,29 @@ void repl(int sock) {
     // }
 
     // receive the current working directory from the server
-    cout << "waiting to recieve cwd" << endl;
-    memset(buffer, 0, MAX_BUFFER_SIZE); // clear the buffer
-    int cwd_received = recv(sock, cwd, MAX_BUFFER_SIZE - 1, 0);
-    if (cwd_received < 0) {
-      cerr << "Error receiving current working directory from server!" << endl;
-      break;
-    } else {
-      cwd[cwd_received] = '\0';
+    // cout << "waiting to recieve cwd" << endl;
+    // memset(buffer, 0, MAX_BUFFER_SIZE); // clear the buffer
+    // int cwd_received = recv(sock, cwd, MAX_BUFFER_SIZE - 1, 0);
+  //   cout << "cwd_received: " << cwd_received << endl;
+  //  if (cwd_received < 0) {
+  //     int errorNumber = errno;      
+  //     cerr << "Error receiving current working directory from server! Error number: " << errorNumber << " Error description: " << strerror(errorNumber) << endl;
+  //     break;
+  //   } else if (cwd_received == 0) {
+  //     cerr << "The server closed the connection." << endl;
+  //     break;
+  //   } else {
+  //     cwd[cwd_received] = '\0';
 
-      // output the current working directory
-      cout << string(cwd) << "-> ";
-    }
+  //     // output the current working directory
+  //     cout << string(cwd) << "-> ";
+  //   }
 
     // get user input
-    cin.getline(buffer, MAX_BUFFER_SIZE);
+    if (!firstTimeCalled){
+      cout << cwd << "->";
+      cin.getline(buffer, MAX_BUFFER_SIZE);
+    }
 
     // close the shell when user enters exit
     if (string(buffer) == "exit") {
@@ -74,11 +129,11 @@ void repl(int sock) {
       break;
     }
 
-    cout << "Sending data to server..." << endl;
+    // cout << "Sending data to server..." << endl;
     // send the user input to the server
     // output error if sending fails
 
-    cout << "sending user input" << endl;
+    // cout << "sending user input" << endl;
     if (send(sock, buffer, strlen(buffer), 0) < 0) {
       cerr << "Error sending data to server!" << endl;
       break;
@@ -88,15 +143,23 @@ void repl(int sock) {
       break;
     }
 
-    cout << "recieving final output" << endl;
+    // cout << "recieving final output" << endl;
     memset(buffer, 0, MAX_BUFFER_SIZE); // 
     if (recv(sock, buffer, MAX_BUFFER_SIZE, 0) < 0) {
       cerr << "Error receiving data from server!" << endl;
-      break;
+      // break;
     }
 
-    cout << buffer << endl;
+    string splitchar = "[$&^]";
+    vector<string> splits = splitString(string(buffer), splitchar);
+    cwd = splits[0];
+    if (!firstTimeCalled){
+      string buffer_str = string(buffer);
 
+      cout << replaceWord(buffer_str, string(cwd+"[$&^]"), string("")) << endl;
+    }else{
+      firstTimeCalled = false;
+    }
   }
  }
 
